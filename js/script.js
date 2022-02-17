3ea3ee7c486bea0f4c511fa1f434ad86cf1884b5
MichaelR <spider747@gmail.com>
2022-02-17 21:08:27 +0200

    Add button go to top

--- js/script.js
+++ js/script.js
@@ -1,130 +1,25 @@
-/* DESCRIPTION: CUSTOM JS FILE */
 
-/* NAVIGATION*/
-// COLLAPSE THE NAVBAR BY ADDING THE TOP-NAV-COLLAPSE CLASS
+// Custom JS script
+const SCROLL_FROM_TOP = 200;    // Defines when button to top will appear
+
+myButton = document.getElementById("btnToTop");
+
+// Show button when user scroll down
 window.onscroll = function () {
-	scrollFunction();
-	scrollFunctionBTT(); // back to top button
-};
-
-function scrollFunction() {
-	let intViewportWidth = window.innerWidth;
-	if (
-		document.body.scrollTop > 30 ||
-		(document.documentElement.scrollTop > 30) & (intViewportWidth > 991)
-	) {
-		document.getElementById("navbar").classList.add("top-nav-collapse");
-	} else if (
-		document.body.scrollTop < 30 ||
-		(document.documentElement.scrollTop < 30) & (intViewportWidth > 991)
-	) {
-		document.getElementById("navbar").classList.remove("top-nav-collapse");
-	}
+    displayButtonToTop();
 }
 
-// NAVBAR ON MOBILE
-let elements = document.querySelectorAll(".nav-link:not(.dropdown-toggle)");
-
-for (let i = 0; i < elements.length; i++) {
-    elements[i].addEventListener("click", () => {
-        document.querySelector(".offcanvas-collapse").classList.toggle("open");
-    });
-}
-
-document.querySelector(".navbar-toggler").addEventListener("click", () => {
-  document.querySelector(".offcanvas-collapse").classList.toggle("open");
-});
-
-// HOVER ON DESKTOP
-function toggleDropdown(e) {
-    const _d = e.target.closest(".dropdown");
-    let _m = document.querySelector(".dropdown-menu", _d);
-
-    setTimeout(
-        function () {
-        const shouldOpen = _d.matches(":hover");
-        _m.classList.toggle("show", shouldOpen);
-        _d.classList.toggle("show", shouldOpen);
-
-        _d.setAttribute("aria-expanded", shouldOpen);
-        },
-        e.type === "mouseleave" ? 300 : 0
-    );
-}
-
-// ON HOVER
-const dropdownCheck = document.querySelector(".dropdown");
-
-if (dropdownCheck !== null) {
-    document
-        .querySelector(".dropdown")
-        .addEventListener("mouseleave", toggleDropdown);
-    document
-        .querySelector(".dropdown")
-        .addEventListener("mouseover", toggleDropdown);
-
-    // ON CLICK
-    document.querySelector(".dropdown").addEventListener("click", (e) => {
-        const _d = e.target.closest(".dropdown");
-        let _m = document.querySelector(".dropdown-menu", _d);
-        if (_d.classList.contains("show")) {
-            _m.classList.remove("show");
-            _d.classList.remove("show");
-        } else {
-            _m.classList.add("show");
-            _d.classList.add("show");
-        }
-    });
-}
-
-/* CARD SLIDER - SWIPER */
-var cardSlider = new Swiper(".card-slider", {
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
-    },
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    slidesPerView: 3,
-    spaceBetween: 70,
-    breakpoints: {
-        // when window is <= 767px
-        767: {
-        slidesPerView: 1,
-        },
-        // when window is <= 991px
-        991: {
-        slidesPerView: 2,
-        spaceBetween: 40,
-        },
-    },
-});
-
-/* BACK TO TOP BUTTON */
-// GET THE BUTTON
-myButton = document.getElementById("myBtn");
-
-// WHEN THE USER SCROLLS DOWN 20PX FROM THE TOP OF THE DOCUMENT, SHOW THE BUTTON
-function scrollFunctionBTT() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+// Set style of button based on scroll amount
+function displayButtonToTop() {
+    if (document.body.scrollTop > SCROLL_FROM_TOP || document.documentElement.scrollTop > SCROLL_FROM_TOP) { /* Handle safair case and others */
         myButton.style.display = "block";
     } else {
         myButton.style.display = "none";
     }
 }
 
-// WHEN THE USER CLICKS ON THE BUTTON, SCROLL TO THE TOP OF THE DOCUMENT
-function topFunction() {
-    document.body.scrollTop = 0; // for Safari
-    document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE and Opera
+// Take to top on button click
+function scrollToTop() {
+    document.body.scrollTop = 0;                // for Safari
+    document.documentElement.scrollTop = 0;     // for Chrome, Firefox, IE and Opera
 }
-
-// AOS ANIMATION ON SCROLL
-AOS.init({
-    duration: 1000,
-    easing: "ease",
-    once: true, // whether animation should happen only once - while scrolling down
-});

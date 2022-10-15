690703eb1be1016b14dee675e60691516713496c
MichaelR <spider747@gmail.com>
2022-10-15 21:10:57 +0300

    Convert app to styled components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import Welcome from './components/Welcome';
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Welcome />
-      </header>
-    </div>
+    <AppStyle>
+      <Welcome />
+    </AppStyle>
   );
 }
+
+const AppStyle = styled.div`
+  text-align: center;
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
 
 export default App;
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,16 +1,40 @@
 import { welcome } from '../assets/Constants';
+import styled from 'styled-components';
 
 const Welcome = () => {
   const {intro, myName, whoAmI, aboutMeText } = welcome;
   return (
-    <div>
+    <WelcomeStyle>
       <p>{intro}</p>
       <h1 className='my-name'>{myName}</h1>
       <h2>{whoAmI}</h2>
       <p>{aboutMeText}</p>
       <h3>View my work</h3>
-    </div>
+    </WelcomeStyle>
   )
 }
+
+const WelcomeStyle = styled.section`
+  .my-name {
+    background: linear-gradient(45deg, #23d5ab, #ee7752, #e73c7e, #23a6d5, #23d5ab, #ee7752, #e73c7e, #23a6d5);
+    background-size: 200% auto;
+    background-clip: text;
+    -webkit-background-clip: text;    /*may need in other browsers*/
+    -webkit-text-fill-color: transparent;
+    animation: changingBackgroundGradient 5s linear infinite;
+  }
+
+  @keyframes changingBackgroundGradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+`
 
 export default Welcome;

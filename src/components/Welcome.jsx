3d3a8e92760913fab463ffde57c7b9f8150e9471
MichaelR <spider747@gmail.com>
2022-10-16 12:38:30 +0300

    Add fade in animation

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -15,6 +15,7 @@ const Welcome = () => {
 }
 
 const WelcomeStyle = styled.section`
+  animation: fadeIn 5s;
   .my-name {
     background: linear-gradient(45deg, #23d5ab, #ee7752, #e73c7e, #23a6d5, #23d5ab, #ee7752, #e73c7e, #23a6d5);
     background-size: 200% auto;
@@ -25,15 +26,14 @@ const WelcomeStyle = styled.section`
   }
 
   @keyframes changingBackgroundGradient {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
+    0% {background-position: 0% 50%;}
+    50% {background-position: 100% 50%;}
+    100% {background-position: 0% 50%;}
+  }
+
+  @keyframes fadeIn {
+    0% {opacity: 0;}
+    100% {opacity: 1;}
   }
 `
 

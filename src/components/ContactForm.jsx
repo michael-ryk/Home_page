77ddc010b9bbf2456e6db227f71704111cf08220
MichaelR <spider747@gmail.com>
2022-11-24 20:49:44 +0200

    improve style of contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -8,18 +8,12 @@ return (
       <form>
         <h2>Please fill your details:</h2>
         <div className='inputs-container'>
-          <div className='input-item'>
-            <label>Your name:</label>
-            <input required='required'/>
-          </div>
-          <div className='input-item'>
-            <label>Your message:</label>
-            <input required='required'/>
-          </div>
-          <div className='input-item'>
-            <label>Email to respond:</label>
-            <input type='email' required='required'/>
-          </div>
+          <label>Your name:</label>
+          <input required='required' name='name' placeholder='John Doe'/>
+          <label>Your Email:</label>
+          <input type='email' required='required' placeholder='example@mail.com' name='email'/>
+          <label>Your message:</label>
+          <textarea required='required' type='text' minLength='20' rows='10' cols='30' placeholder='Type your message here...' name='message'/>
         </div>
         <div className='buttons-container'>
           <button>Submit</button>
@@ -35,24 +29,23 @@ const ModalStyles = styled.div`
 margin: 0 auto;
 width: 100%;
 form {
-  h2 {
-    text-decoration: underline;
-  }
   .inputs-container {
     display: flex;
     flex-direction: column;
     justify-self: left;
-    .input-item {
-      margin: 0.5rem auto;
-      label{
-        font-size: 1.3rem;
-        margin-right: 0.5rem;
-      }
-      input {
-        color: black;
-        font-size: 1.2rem;
-        text-align: left;
-      }
+    max-width: 500px;
+    margin: 0.5rem auto;
+    font-size: 1.3rem;
+    label{
+      margin: 0.5rem 0 0.25rem;
+      text-align: left;
+    }
+    input,
+    textarea {
+      color: black;
+      font-size: 1.2rem;
+      text-align: left;
+      padding: 0.3rem;
     }
   }
   .buttons-container {
